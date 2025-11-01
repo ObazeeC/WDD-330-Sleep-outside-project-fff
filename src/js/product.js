1f1d6f4b396b99be9e617b5437a9f99b83afa47c
obazeec <obazee.esosa@yahoo.com>
2025-11-01 00:17:48 +0000

    Add item to cart completed

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,17 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //  setLocalStorage("so-cart", product); this was just overwriting new items added
+  //correct code below to add multiple items
+
+  // Get existing cart or empty array
+  const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+
+  // add new item to it
+  cart.push(product);
+
+  // save updated cart
+  localStorage.setItem("so-cart", JSON.stringify(cart));
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
