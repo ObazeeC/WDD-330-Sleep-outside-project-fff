bfa6e57949ded696de53f8d404ed5deb65684338
ezekiel-spec <froshd52@gmail.com>
2025-11-13 12:01:58 +0100

    Fix local changes after merge

--- vite.config.js
+++ vite.config.js
@@ -11,23 +11,20 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-<<<<<<< HEAD
         product: resolve(__dirname, "src/product_pages/index.html"),
-=======
         product1: resolve(
           __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
+          "src/product_pages/cedar-ridge-rimrock-2.html"
         ),
         product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
         product3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html",
+          "src/product_pages/northface-alpine-3.html"
         ),
         product4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html",
+          "src/product_pages/northface-talus-4.html"
         ),
->>>>>>> 7c71629eb3eeb38605fe6f8354937ac79f808ad6
       },
     },
   },
